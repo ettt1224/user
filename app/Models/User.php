cc77792b86e7ccd7ac53a1f6242be0f05ff001bf
ettt1224 <ettt1224@gmail.com>
2026-02-24 16:36:23 +0800

    更

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\softDeletes;
+use Illuminate\Database\Eloquent\Factories\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, SoftDeletes;
