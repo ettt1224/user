0122ff1415a96f481b8f138381dd7d4955175c66
ettt1224 <ettt1224@gmail.com>
2026-02-24 16:03:59 +0800

    service功能

---
+++ app/service/Userservice.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    public function getAllUsers()
+    {
+        return User::all();
+    }
+
+    public function createUser(array $data): User
+    {
+
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        return User::create($data);
+    }
+
+    
+    public function updateUser(User $user, array $data): User
+    {
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        $user->update($data);
+        return $user;
+    }
+
+    public function deleteUser(User $user): void
+    {
+        $user->delete();
+    }
+}
